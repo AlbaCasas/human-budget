3b0ebcfecaa9201df661ffd40ccb3830cdcce8e0
ac <ac@MacBook-Pro-de-Jordi.local>
2021-10-29 17:47:36 +0200

    Create component files

---
+++ src/components/IconCard/IconCard.jsx
@@ -0,0 +1,8 @@
+const IconCard = (props) => {
+    return <div>
+        <p>Hola</p>
+        <p>Hola</p>
+    </div>;
+}
+
+export default IconCard;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 import Layout from "../components/Layout";
 import ExpenseCard from "../components/ExpenseCard";
+import IconCard from "../components/IconCard/";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <ExpenseCard title="Hola" date="12 de Octubre, 2021" amount="400$" />
+        <IconCard/>
       </Layout>
     </div>
   );
