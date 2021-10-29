385d790d2abcb95a6bf3100570a962180d02b43d
ac <ac@MacBook-Pro-de-Jordi.local>
2021-10-29 20:23:05 +0200

    Add expenses heading to App

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,17 @@
 import Layout from "../components/Layout";
 import ExpenseCard from "../components/ExpenseCard";
+import Title from "../components/Title";
+import Button from "../components/Button";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Layout>
+        <div className="app__heading">
+          <Title>Expenses</Title>
+          <Button icon="plus" variant="default" text="Add Expense"/>
+        </div>
         <ExpenseCard title="Hola" date="12 de Octubre, 2021" amount="400$" />
       </Layout>
     </div>
