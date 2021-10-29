f9848b7047693462704a0868ea44b4b87713dfaf
ac <ac@MacBook-Pro-de-Jordi.local>
2021-10-29 18:45:00 +0200

    refactor icon component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
 import Icon from "../Icon";
-import getPath, { getClassName } from "./utils";
+import { getClassName } from "./utils";
 import "./Button.css";
 
 const Button = (props) => {
     return <button className={getClassName(props.variant)}>
         {props.text}
-        <Icon path={getPath(props.icon)} />
+        <Icon icon={props.icon} />
     </button>
 }
 
