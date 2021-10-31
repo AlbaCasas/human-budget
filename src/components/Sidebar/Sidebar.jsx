5729798922a10c56fba88f5114812e641c2ab96a
ac <ac@MacBook-Pro-de-Jordi.local>
2021-10-31 17:38:49 +0100

    Create income modal with onClick events

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Title from "../Title";
 import Button from "../Button";
+import CreateIncomeModal from "../CreateIncomeModal/";
 import "./Sidebar.css";
 
 const Sidebar = (props) => {
+    const [isIncomeModalShown, setIsIncomeModalShown] = useState(false);
     return <aside className={`sidebar ${props.className}`}>
+        <CreateIncomeModal onCloseClick={() => {
+            setIsIncomeModalShown(false);
+        }} isShown={isIncomeModalShown}/>
         <div className="sidebar__heading">
             <Title>Incomes</Title>
-            <Button icon="plus" variant="default"/>
+            <Button onClick={() => {
+                setIsIncomeModalShown(true);
+            }} icon="plus" variant="default"/>
         </div>
     </aside>;
 }
