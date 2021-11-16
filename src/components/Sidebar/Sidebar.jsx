d8d396becca31d93173033705f4e282cf2f45c7f
Alba Casas <albacasasarzua1b@gmail.com>
2021-11-16 08:15:56 +0100

    Formatting with prettier

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,18 +5,27 @@ import CreateIncomeModal from "../CreateIncomeModal/";
 import "./Sidebar.css";
 
 const Sidebar = (props) => {
-    const [isIncomeModalShown, setIsIncomeModalShown] = useState(false);
-    return <aside className={`sidebar ${props.className}`}>
-        <CreateIncomeModal onCloseClick={() => {
-            setIsIncomeModalShown(false);
-        }} isShown={isIncomeModalShown}/>
-        <div className="sidebar__heading">
-            <Title>Incomes</Title>
-            <Button onClick={() => {
-                setIsIncomeModalShown(true);
-            }} icon="plus" variant="default"/>
-        </div>
-    </aside>;
-}
+  const [isIncomeModalShown, setIsIncomeModalShown] = useState(false);
+  return (
+    <aside className={`sidebar ${props.className}`}>
+      <CreateIncomeModal
+        onCloseClick={() => {
+          setIsIncomeModalShown(false);
+        }}
+        isShown={isIncomeModalShown}
+      />
+      <div className="sidebar__heading">
+        <Title>Incomes</Title>
+        <Button
+          onClick={() => {
+            setIsIncomeModalShown(true);
+          }}
+          icon="plus"
+          variant="default"
+        />
+      </div>
+    </aside>
+  );
+};
 
 export default Sidebar;
