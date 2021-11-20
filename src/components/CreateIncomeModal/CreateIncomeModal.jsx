a83fbe91c4c04efbc0e488da7f3096f4b9a5d577
Alba Casas <albacasasarzua1b@gmail.com>
2021-11-20 16:45:00 +0100

    Allow creating incomes from IncomModal

--- src/components/CreateIncomeModal/CreateIncomeModal.jsx
+++ src/components/CreateIncomeModal/CreateIncomeModal.jsx
@@ -5,16 +5,41 @@ import Title from "../Title";
 import "./CreateIncomeModal.css";
 
 const CreateIncomeModal = (props) => {
-    return props.isShown === true ? (
-        <Modal onCloseClick={props.onCloseClick} className="income-modal">
-            <Title>Add income</Title>
-            <div className="income-modal__form">
-                <Input className="income-modal__input income-modal__input--name" placeholder="income name"/>
-                <Input className="income-modal__input income-modal__input--amount" placeholder="$ Amount" type="number"/>
-                <Button variant="default" icon="plus"/>
-            </div>
-        </Modal>
-    ) : null;
-}
+  const handleIncomeSubmit = (event) => {
+    event.preventDefault();
+    const titleValue = event.target["title"].value;
+    const amountValue = event.target["amount"].value;
+    if (titleValue !== "" && amountValue !== "") {
+      props.setIncomes([
+        ...props.incomes,
+        {
+          title: titleValue,
+          date: new Date().toISOString(),
+          amount: amountValue,
+          icon: "home",
+        },
+      ]);
+    }
+  };
+  return props.isShown === true ? (
+    <Modal onCloseClick={props.onCloseClick} className="income-modal">
+      <Title>Add income</Title>
+      <form className="income-modal__form" onSubmit={handleIncomeSubmit}>
+        <Input
+          name="title"
+          className="income-modal__input income-modal__input--name"
+          placeholder="income name"
+        />
+        <Input
+          name="amount"
+          className="income-modal__input income-modal__input--amount"
+          placeholder="$ Amount"
+          type="number"
+        />
+        <Button variant="default" icon="plus" />
+      </form>
+    </Modal>
+  ) : null;
+};
 
 export default CreateIncomeModal;
