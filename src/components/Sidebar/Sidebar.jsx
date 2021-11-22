e0738f4e885fe65950dd9fbc33d5b0d9939541a9
Alba Casas <albacasasarzua1b@gmail.com>
2021-11-22 20:02:37 +0100

    Refactor global components to make them composable

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,34 +1,13 @@
-import Title from "../Title";
-import Button from "../Button";
 import "./Sidebar.css";
-import ExpenseCard from "../ExpenseCard";
 
 const Sidebar = (props) => {
   return (
-    <aside className={`sidebar ${props.className}`}>
-      <div className="sidebar__heading">
-        <Title>Incomes</Title>
-        <Button
-          onClick={() => {
-            props.setIsIncomeModalShown(true);
-          }}
-          icon="plus"
-          variant="default"
-        />
-      </div>
-      {props.incomes.map((item) => {
-        return (
-          <ExpenseCard
-            title={item.title}
-            date={item.date}
-            amount={item.amount}
-            icon={item.icon}
-            color={item.color}
-          />
-        );
-      })}
-    </aside>
+    <aside className={`sidebar ${props.className}`}>{props.children}</aside>
   );
+};
+
+export const SidebarHeading = (props) => {
+  return <div className="sidebar__heading">{props.children}</div>;
 };
 
 export default Sidebar;
--- src/pages/App.js
+++ src/pages/App.js
@@ -8,6 +8,12 @@ import spentSvg from "../assets/spent.svg";
 import bankSvg from "../assets/bank.svg";
 import "./App.css";
 import Expenses from "../blocks/Expenses";
+import Sidebar from "../components/Sidebar";
+import { SidebarHeading } from "../components/Sidebar/Sidebar";
+import ExpenseCard from "../components/ExpenseCard";
+import { LayoutContent, LayoutNav } from "../components/Layout/Layout";
+import Title from "../components/Title";
+import Button from "../components/Button";
 
 const App = () => {
   const [toastMessage, setToastMessage] = useState(null);
@@ -27,18 +33,42 @@ const App = () => {
         isShown={isIncomeModalShown}
       />
       <Layout incomes={incomes} setIsIncomeModalShown={setIsIncomeModalShown}>
-        <div className="app__header">
-          <TotalAmount text="Amount left to spend" img={moneySvg}>
-            600
-          </TotalAmount>
-          <TotalAmount text="Spent amount" img={spentSvg}>
-            400
-          </TotalAmount>
-          <TotalAmount text="Original amount" img={bankSvg}>
-            1000
-          </TotalAmount>
-        </div>
-        <Expenses setToastMessage={setToastMessage} />
+        <LayoutContent>
+          <LayoutNav />
+          <div className="app__header">
+            <TotalAmount text="Amount left to spend" img={moneySvg}>
+              600
+            </TotalAmount>
+            <TotalAmount text="Spent amount" img={spentSvg}>
+              400
+            </TotalAmount>
+            <TotalAmount text="Original amount" img={bankSvg}>
+              1000
+            </TotalAmount>
+          </div>
+          <Expenses setToastMessage={setToastMessage} />
+        </LayoutContent>
+        <Sidebar>
+          <SidebarHeading>
+            <Title>Incomes</Title>
+            <Button
+              onClick={() => setIsIncomeModalShown(true)}
+              icon="plus"
+              variant="default"
+            />
+          </SidebarHeading>
+          {incomes.map((item) => {
+            return (
+              <ExpenseCard
+                title={item.title}
+                date={item.date}
+                amount={item.amount}
+                icon={item.icon}
+                color={item.color}
+              />
+            );
+          })}
+        </Sidebar>
       </Layout>
     </div>
   );
